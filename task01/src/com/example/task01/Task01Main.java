e14e73de420a0204506a21f696398757005ec6c9
BMSalamander <hostan3@gmail.com>
2018-10-21 21:43:34 +0500

    undo

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,28 +2,6 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
-        Logger fLogger = Logger.getLogger("test1");
-        Logger sLogger = Logger.getLogger("test2");
-        Logger afLogger = Logger.getLogger("test1");
-
-        if (fLogger == afLogger) {
-            System.out.println("One instance of fLogger!");
-        }
-
-        System.out.println(fLogger.getLevel());
-        fLogger.setLevel(ImportanceLevel.WARNING);
-        System.out.println(fLogger.getLevel());
-
-        fLogger.warning("some message");
-        fLogger.warning("%d %d %s", 4, 5, "some !");
-
-        fLogger.debug("will not printed");
-        sLogger.debug("will be printed");
-
-        fLogger.log("message info not printed", ImportanceLevel.INFO);
-        fLogger.log("message info is printed", ImportanceLevel.ERROR);
-
-        fLogger.log("formatted %d %d %f", ImportanceLevel.WARNING, 4, 7, 2.5);
 
     }
 }
