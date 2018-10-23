4ede358193fa596afd92d37b83cbc4a6cce4cf2a
Leva <petrosian.lyova@yandex.ru>
2018-10-23 21:43:14 +0500

    Петросян. Task01

--- task01/src/com/example/task01/Logger.java
+++ task01/src/com/example/task01/Logger.java
@@ -6,40 +6,29 @@ import java.text.SimpleDateFormat;
 
 public class Logger {
 
-    private String name;
-    private levels level;
+    private final String name;
+    private Level level;
     private static Map<String, Logger> loggers = new HashMap<String, Logger>();
 
-    public enum levels {
+    public enum Level {
         DEBUG,
         INFO,
         WARNING,
         ERROR,
     }
 
-    public Logger() {
-        this.name = levels.DEBUG.toString();
-        this.level = levels.DEBUG;
-
-        loggers.put(this.name, this);
-    }
-
-    public Logger(String name) {
+    Logger(String name) {
         this.name = name;
         loggers.put(this.name, this);
     }
 
-    public Logger(String name, levels level) {
+    private Logger(String name, Level level) {
         this.name = name;
         this.level = level;
         loggers.put(this.name, this);
     }
 
-    /*public void setName(String name) {
-        this.name = name;
-    }*/
-
-    public void setLevel(levels level) {
+    public void setLevel(Level level) {
         this.level = level;
     }
 
@@ -47,67 +36,64 @@ public class Logger {
         return this.name;
     }
 
-    public levels getLevel() {
+    public Level getLevel() {
         return this.level;
     }
 
     public static Logger getLogger(String name) {
 
-        if (loggers.get(name) != null)
-            return loggers.get(name);
+        Logger temp = loggers.get(name);
+        if (temp != null)
+            return temp;
 
-        loggers.put(name, new Logger(name));
-        return loggers.get(name);
+        temp = loggers.put(name, new Logger(name));
+        return temp;
     }
 
     public void debug(String message) {
-        LogMessage(levels.DEBUG, message);
+        logMessage(Level.DEBUG, message);
     }
 
     public void debug(String strFormat, Object... args) {
         String message = String.format(strFormat, args);
-        LogMessage(levels.DEBUG, message);
+        logMessage(Level.DEBUG, message);
     }
 
     public void info(String message) {
-        LogMessage(levels.INFO, message);
+        logMessage(Level.INFO, message);
     }
 
     public void info(String strFormat, Object... args) {
         String message = String.format(strFormat, args);
-        LogMessage(levels.INFO, message);
+        logMessage(Level.INFO, message);
     }
 
     public void warning(String message) {
-        LogMessage(levels.WARNING, message);
+        logMessage(Level.WARNING, message);
     }
 
     public void warning(String strFormat, Object... args) {
         String message = String.format(strFormat, args);
-        LogMessage(levels.WARNING, message);
+        logMessage(Level.WARNING, message);
     }
 
     public void error(String message) {
-        LogMessage(levels.ERROR, message);
+        logMessage(Level.ERROR, message);
     }
 
     public void error(String strFormat, Object... args) {
         String message = String.format(strFormat, args);
-        LogMessage(levels.ERROR, message);
+        logMessage(Level.ERROR, message);
     }
 
-    public void LogMessage(levels level, String message) {
+    public void logMessage(Level level, String message) {
         //[WARNING] 2018.07.17 09:56:32 myLogger - something weird happened
-        if (isLoggable(level) && level.ordinal() >= this.level.ordinal()) {
+        if (level != null && level.ordinal() >= this.level.ordinal()) {
             DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd hh:mm:ss");
             Date date = new Date();
             String log = String.format("[%s] %s %s - %s", level.toString(), dateFormat.format(date), name, message);
             System.out.println(log);
         }
-    }
-
-    public boolean isLoggable(levels level) {
-        return level != null;
     }
 
 }
