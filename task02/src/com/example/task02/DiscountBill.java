447d83a54d1314a8879cf50024984d0e8fcf1dbf
AnnaAnvok <zakiryanova97@bk.ru>
2018-10-26 02:11:49 +0500

    Закирьянова А. Задание 02

---
+++ task02/src/com/example/task02/DiscountBill.java
@@ -0,0 +1,40 @@
+package com.example.task02;
+
+public class DiscountBill
+        extends Bill {
+
+    private int Discount;
+
+    public int GetDiscount() {
+        return Discount;
+    }
+
+    public void setDiscount(int Discount) {
+        if (Discount < 0 || Discount > 100) {
+            throw new AssertionError("Таких скидок не бывает:(.");
+        } else {
+            this.Discount = Discount;
+        }
+    }
+
+    @Override
+    public long getPrice() {
+        long DiscountPrice = super.getPrice();
+        DiscountPrice *= (100 - Discount);
+        return DiscountPrice / 100;
+    }
+
+    public long advantage() {
+        return super.getPrice() - this.getPrice();
+    }
+
+    @Override
+    public String toString() {
+        if (Discount == 0) {
+            return super.toString();
+        }
+        else {
+            return super.toString() + " сo скидкой " + Discount + " %";
+        }
+    }
+}
