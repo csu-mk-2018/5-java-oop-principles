88cec2c95620961f7e6ba25caeb11c7807471766
DanilaKroshin <skozlozopsk74@gmail.com>
2018-12-25 12:43:26 +0500

    Крошин Д.И. task01 исправлено

--- task01/src/com/example/task01/Logger.java
+++ task01/src/com/example/task01/Logger.java
@@ -4,82 +4,116 @@ import java.util.*;
 import java.text.*;
 
 public class Logger {
-    private final String name;
-    private Level level = Level.DEBUG;
-    private static HashMap<String, Logger> loggers = new HashMap<>();
 
-    public static enum Level {
+    public enum Level {
         DEBUG,
         INFO,
         WARNING,
         ERROR
     }
 
+    private final String name;
+    private Level impLevel = Level.DEBUG;
+
+    private static HashMap<String, Logger> loggers = new HashMap<>();
+
     private Logger(String name) {
         this.name = name;
-        loggers.put(name, this);
+    }
+
+    private static Logger createLogger(String name) {
+        Logger newLogger = new Logger(name);
+        loggers.put(name, newLogger);
+        return newLogger;
+    }
+
+    public static Logger getLogger(String name) {
+        Logger logger = loggers.get(name);
+        return logger == null ? createLogger(name) : logger;
     }
 
     public String getName() {
         return name;
     }
 
-    public void error(String msg) {
-        log(Level.ERROR, msg);
+    private static String getNowDateFormat() {
+        Date dateNow = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("yyyy.MM.dd hh:mm:ss");
+        return formatForDateNow.format(dateNow);
     }
 
-    public void error(String format, Object... varargs) {
-        log(Level.ERROR, String.format(format, varargs));
+    private void printMessage(String message, Level impLevel) {
+        // printableMessage = "[" + impLevel.name() + "] " + getNowDateFormat() + String.format(" %s - %s", name, message);
+        String printableMessage = String.format("[%s] %s %s - %s", impLevel.name(), getNowDateFormat(), name, message);
+        System.out.println(printableMessage);
     }
 
-    public void warning(String msg) {
-        log(Level.WARNING, msg);
-    }
-
-    public void warning(String format, Object... varargs) {
-        log(Level.WARNING, String.format(format, varargs));
-    }
-
-    public void info(String msg) {
-        log(Level.INFO, msg);
-    }
-
-    public void info(String format, Object... varargs) {
-        log(Level.INFO, String.format(format, varargs));
-    }
-
-    public void debug(String msg) {
-        log(Level.DEBUG, msg);
-    }
-
-    public void debug(String format, Object... varargs) {
-        log(Level.DEBUG, String.format(format, varargs));
-    }
-
-    public void log(Level level, String message) {
-        if (level.ordinal() >= this.level.ordinal()) {
-            String dateNow = new SimpleDateFormat("yyyy.MM.dd hh:mm:ss").format(new Date());
-            System.out.println(String.format("[%s] %s %s - %s", level, dateNow, name, message));
+    public void debug(String message) {
+        if (impLevel.ordinal() <= Level.DEBUG.ordinal()) {
+            printMessage(message, Level.DEBUG);
         }
     }
 
-    public void log(Level level, String format, Object... varargs) {
-        log(level, String.format(format, varargs));
+    public void debug(String format, Object... params) {
+        if (impLevel.ordinal() <= Level.DEBUG.ordinal()) {
+            printMessage(String.format(format, params), Level.DEBUG);
+        }
     }
 
-    public static Logger getLogger(String name) {
-        Logger logger = loggers.get(name);
-        if (logger == null) {
-            logger = new Logger(name);
+    public void info(String message) {
+        if (impLevel.ordinal() <= Level.INFO.ordinal()) {
+            printMessage(message, Level.INFO);
         }
-        return logger;
+    }
+
+    public void info(String format, Object... params) {
+        if (impLevel.ordinal() <= Level.INFO.ordinal()) {
+            printMessage(String.format(format, params), Level.INFO);
+        }
+    }
+
+    public void warning(String message) {
+        if (impLevel.ordinal() <= Level.WARNING.ordinal()) {
+            printMessage(message, Level.WARNING);
+        }
+    }
+
+    public void warning(String format, Object... params) {
+        if (impLevel.ordinal() <= Level.WARNING.ordinal()) {
+            printMessage(String.format(format, params), Level.WARNING);
+        }
+    }
+
+    public void error(String message) {
+        if (impLevel.ordinal() <= Level.ERROR.ordinal()) {
+            printMessage(message, Level.ERROR);
+        }
+    }
+
+    public void error(String format, Object... params) {
+        if (impLevel.ordinal() <= Level.ERROR.ordinal()) {
+            printMessage(String.format(format, params), Level.ERROR);
+        }
+    }
+
+    public void log(String message, Level impLevel) {
+        if (this.impLevel.ordinal() <= impLevel.ordinal()) {
+            printMessage(message, impLevel);
+        }
+    }
+
+    public void log(String format, Level impLevel, Object... params) {
+        if (this.impLevel.ordinal() <= impLevel.ordinal()) {
+            printMessage(String.format(format, params), impLevel);
+        }
     }
 
     public Level getLevel() {
-        return level;
+        return impLevel;
     }
 
-    public void setLevel(Level level) {
-        this.level = level;
+    public void setLevel(Level impLevel) {
+        this.impLevel = impLevel;
     }
+
 }
